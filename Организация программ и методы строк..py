b2ce82abb311f6dc885b6afcbe4d627337a6398d
Qvait <brylov.1996@mail.ru>
2024-07-27 23:30:32 +0500

    Add files via upload

---
+++ Организация программ и методы строк..py
@@ -0,0 +1,12 @@
+#Выведите строку my_string в верхнем регистре.
+#Выведите строку my_string в нижнем регистре.
+#Измените строку my_string, удалив все пробелы.
+#Выведите первый символ строки my_string.
+#Выведите последний символ строки my_string.
+my_string=input()
+print(my_string. upper())
+print(my_string. lower())
+print(my_string. replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+print(len(my_string))
